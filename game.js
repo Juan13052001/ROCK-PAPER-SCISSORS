9f2ff198fb8e8ed4cc0979efc0d230672996e4ab
Juan <jherrerav1901@alumno.ipn.mx>
2022-04-07 17:53:09 -0500

    Se agrego 3 botones con sus correspondientes id Se agrego eventos en el DOM

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
+const body = document.querySelector("body");
 function computerPlay() {
     const rockPaperScissors = ["Rock", "Paper", "Scissors"];
     let random = Math.floor(Math.random() * rockPaperScissors.length);
     return rockPaperScissors[random];
 }
+
 let contadorSistema = 0;
+
 let contadorJugador = 0;
+
 function playRound(playerSelection, computerSelection) {
     let computerPlay = computerSelection.toLowerCase();
+
     let selectionPlayer = playerSelection.toLowerCase();
+    console.log(computerPlay);
+    console.log(selectionPlayer);
+
     let ganador = "";
     if (
         (selectionPlayer === "scissors" && computerPlay === "rock") ||
@@ -16,32 +24,67 @@ function playRound(playerSelection, computerSelection) {
     ) {
         ganador = "Has perdido";
         contadorSistema++;
-        return `Sistema: ${contadorSistema}`;
+        console.log(`Sistema: ${contadorSistema}`);
     } else if (
         (selectionPlayer === "paper" && computerPlay === "paper") ||
         (selectionPlayer === "scissors" && computerPlay === "scissors") ||
         (selectionPlayer === "rock" && computerPlay === "rock")
     ) {
         ganador = "Empate";
-        return ganador;
+        console.log(ganador);
     } else {
         ganador = "Has ganado";
         contadorJugador++;
-        return `Jugador: ${contadorJugador}`;
-    }
-    // console.log(
-    //     contadorJugador > contadorSistema
-    //         ? `Has ganado con ${contadorJugador} puntos`
-    //         : `Has perdido`
-    // );
-}
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let eleccion = prompt("Escoge una opción: Paper, Rock, Scissors");
-        console.log(eleccion);
-        let juego = playRound(eleccion, computerPlay());
-        console.log(juego);
+        console.log(`Jugador: ${contadorJugador}`);
     }
 }
 
-game();
+// *Seleccionar el div container para insertar mis botones
+const container = document.querySelector(".container");
+
+//*Crear tres botones
+const buttonPiedra = document.createElement("button");
+
+const buttonPapel = document.createElement("button");
+
+const buttonTijeras = document.createElement("button");
+//*Insertar el texto que debe de tener cada uno
+buttonPiedra.innerText = "Piedra";
+
+buttonPapel.innerText = "Papel";
+
+buttonTijeras.innerText = "Tijeras";
+
+//*Insertarles un id a los botones
+
+buttonPiedra.setAttribute("id", "rock");
+
+buttonPapel.setAttribute("id", "paper");
+
+buttonTijeras.setAttribute("id", "scissors");
+
+container.append(buttonPiedra, buttonPapel, buttonTijeras);
+
+// console.log(
+//     contadorJugador > contadorSistema
+//         ? `Has ganado con ${contadorJugador} puntos`
+//         : `Has perdido`
+// );
+// }
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         let eleccion = prompt("Escoge una opción: Paper, Rock, Scissors");
+//         console.log(eleccion);
+//         let juego = playRound(eleccion, computerPlay());
+//         console.log(juego);
+//     }
+// }
+
+// game();
+let boton = document.querySelectorAll("button");
+console.log(boton);
+
+body.addEventListener("click", (e) => {
+    let eleccion = e.target.id;
+    playRound(eleccion, computerPlay());
+});
