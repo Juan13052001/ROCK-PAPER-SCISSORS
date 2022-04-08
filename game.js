35745bc47a9b5801d5e9eea1282f9dbbb0287a61
Juan <jherrerav1901@alumno.ipn.mx>
2022-04-07 19:24:45 -0500

    Agregado estilos y funcionalidad

--- game.js
+++ game.js
@@ -1,90 +1,47 @@
-const body = document.querySelector("body");
+let puntajeUsuario = 0;
+let puntajeSistema = 0;
+let resultados = document.getElementById("result");
+let botones = document.querySelectorAll("input");
+
+function blockButtton(e) {
+    botones.forEach((boton) => (boton.disabled = true));
+}
+
 function computerPlay() {
-    const rockPaperScissors = ["Rock", "Paper", "Scissors"];
-    let random = Math.floor(Math.random() * rockPaperScissors.length);
-    return rockPaperScissors[random];
+    let rps = ["rock", "paper", "scissors"];
+    return rps[Math.floor(Math.random() * rps.length)];
 }
 
-let contadorSistema = 0;
-
-let contadorJugador = 0;
-
-function playRound(playerSelection, computerSelection) {
-    let computerPlay = computerSelection.toLowerCase();
-
-    let selectionPlayer = playerSelection.toLowerCase();
-    console.log(computerPlay);
-    console.log(selectionPlayer);
-
-    let ganador = "";
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
+    let resultado = "";
     if (
-        (selectionPlayer === "scissors" && computerPlay === "rock") ||
-        (selectionPlayer === "rock" && computerPlay === "paper") ||
-        (selectionPlayer === "paper" && computerPlay === "scissors")
+        (playerSelection === "scissors" && computerSelection === "rock") ||
+        (playerSelection === "rock" && computerSelection === "paper") ||
+        (playerSelection === "paper" && computerSelection === "scissors")
     ) {
-        ganador = "Has perdido";
-        contadorSistema++;
-        console.log(`Sistema: ${contadorSistema}`);
-    } else if (
-        (selectionPlayer === "paper" && computerPlay === "paper") ||
-        (selectionPlayer === "scissors" && computerPlay === "scissors") ||
-        (selectionPlayer === "rock" && computerPlay === "rock")
-    ) {
-        ganador = "Empate";
-        console.log(ganador);
+        puntajeSistema++;
+        resultado = `Has perdido! ${computerSelection} es mejor que ${playerSelection} <br/>Computadora: ${puntajeSistema}<br/>Usuario: ${puntajeUsuario}`;
+        if (puntajeSistema === 5) {
+            resultado = `<br/>Has perdido el juego, recarga la pagina para intentarlo de nuevo`;
+            blockButtton();
+        }
+    } else if (playerSelection === computerSelection) {
+        resultado = `<br/> Empate, Escoge otra vez.<br/> Usuario: ${puntajeUsuario}<br/>Sistema: ${puntajeSistema}`;
     } else {
-        ganador = "Has ganado";
-        contadorJugador++;
-        console.log(`Jugador: ${contadorJugador}`);
+        puntajeUsuario++;
+        resultado = `Ganaste,${playerSelection} es mejor que ${computerSelection} <br/>Computadora: ${puntajeSistema}<br/>Usuario: ${puntajeUsuario}`;
+        if (puntajeUsuario === 5) {
+            resultado = `<br/>Has ganado el juego, recarga la pagina para hacerlo de nuevo`;
+            blockButtton();
+        }
     }
+    return (resultados.innerHTML = resultado);
 }
 
-// *Seleccionar el div container para insertar mis botones
-const container = document.querySelector(".container");
-
-//*Crear tres botones
-const buttonPiedra = document.createElement("button");
-
-const buttonPapel = document.createElement("button");
-
-const buttonTijeras = document.createElement("button");
-//*Insertar el texto que debe de tener cada uno
-buttonPiedra.innerText = "Piedra";
-
-buttonPapel.innerText = "Papel";
-
-buttonTijeras.innerText = "Tijeras";
-
-//*Insertarles un id a los botones
-
-buttonPiedra.setAttribute("id", "rock");
-
-buttonPapel.setAttribute("id", "paper");
-
-buttonTijeras.setAttribute("id", "scissors");
-
-container.append(buttonPiedra, buttonPapel, buttonTijeras);
-
-// console.log(
-//     contadorJugador > contadorSistema
-//         ? `Has ganado con ${contadorJugador} puntos`
-//         : `Has perdido`
-// );
-// }
-// function game() {
-//     for (let i = 0; i < 5; i++) {
-//         let eleccion = prompt("Escoge una opción: Paper, Rock, Scissors");
-//         console.log(eleccion);
-//         let juego = playRound(eleccion, computerPlay());
-//         console.log(juego);
-//     }
-// }
-
-// game();
-let boton = document.querySelectorAll("button");
-console.log(boton);
-
-body.addEventListener("click", (e) => {
-    let eleccion = e.target.id;
-    playRound(eleccion, computerPlay());
+botones.forEach((boton) => {
+    boton.addEventListener("click", (e) => {
+        let eleccion = e.target.value;
+        playRound(eleccion);
+    });
 });
